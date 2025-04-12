e6e93507f009d9b4f49f89f2aaa1939168dec54b
salmamehrez85 <salmamehrez85@gmail.com>
2025-04-12 05:45:55 +0200

    Vue Setup + NavBar Component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import NavBar from "./components/NavBar.vue";
+</script>
+
+<template>
+  <NavBar />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ["Product", "Solutions", "Pricing", "Resources", "Editorial"],
+    };
+  },
+  components: {
+    NavBar,
+  },
+};
+</script>
+<style scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import logo from "@/assets/logo.webp";
+</script>
+
+<template>
+  <div class="py-4 px-30 flex justify-between">
+    <img class="w-1/11" :src="logo" alt="logo" />
+    <div class="flex">
+      <p
+        class="px-4 rounded-lg hover:underline hover:bg-gray-100 cursor-pointer"
+        v-for="navbaritem in items"
+      >
+        {{ navbaritem }}
+      </p>
+    </div>
+
+    <div class="flex gap-7">
+      <button class="hover:underline cursor-pointer">Log in</button>
+      <button
+        class="cursor-pointer px-4 py-1 rounded-xl bg-yellow-200 hover:bg-yellow-300"
+      >
+        Start For Free
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ["Product", "Solutions", "Pricing", "Resources", "Editorial"],
+    };
+  },
+};
+</script>
+<style scoped></style>
